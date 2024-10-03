7284a27c7aaba9a0404091b12718047c1affe1e5
Naeem Ashraf <123843315+NP5555@users.noreply.github.com>
2024-10-03 14:34:02 -0700

    nthg

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,14 @@ const Footer = () => {
   return (
     <div>
       <div className="items-center mt-5">
-        <pre className="mb-4 font-bold rounded-xl text-center text-[#a2d6e9]">
+        <pre className="mb-4 font-bold rounded-xl text-center italic text-[#a2d6e9]">
           All rights Reserved{" "}
           <a
             href="https://github.com/NP5555"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <span className="text-sm text-yellow-300   ">GitHub</span>
+            <span className="text-sm text-yellow-300 ">GitHub</span>
           </a>
         </pre>
       </div>
