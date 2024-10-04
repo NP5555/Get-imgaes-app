8eaa687355de8a33a2b15828479b9c57d45898ac
Naeem Ashraf <123843315+NP5555@users.noreply.github.com>
2024-10-04 09:11:06 -0700

    nthg"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <div className="justify-center items-center flex flex-row p-3">
 <div className="mb-3">
-<img src={logo} className=" md:h-10 text-fuchsia-500 " />
+<img src={logo} alt="logoGetImage_" className=" md:h-10 text-fuchsia-500 " />
 </div>
 <div>
 <h1 className=" text-gray-400 text-center text-2xl mx-auto font-extrabold">
